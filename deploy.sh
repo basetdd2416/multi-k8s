0e9ab122535797ab83b4387419c3e83ed3c5abf8
Apisit Onakekasit <blue@appsynth.net>
2018-11-07 16:41:08 +0700

    fix typo shell script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
+
 docker build -t bluesaluman/multi-client:latest -t bluesaluman/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t bluesaluman/multi-server:latest -t bluesaluman/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t bluesaluman/multi-worker -t -t bluesaluman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t bluesaluman/multi-worker:latest -t bluesaluman/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push bluesaluman/multi-client:latest
+docker push bluesaluman/multi-server:latest
+docker push bluesaluman/multi-worker:latest
+
 docker push bluesaluman/multi-client:$SHA
-docker push bluesaluman/multi-server$SHA
-docker push bluesaluman/multi-worker$SHA
+docker push bluesaluman/multi-server:$SHA
+docker push bluesaluman/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=bluesaluman/multi-server:$SHA
 kubectl set image deployments/client-deployment client=bluesaluman/multi-client:$SHA
